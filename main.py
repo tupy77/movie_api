243b2f66b7639579729b0826e2a05db9c54f6bfb
tupy77 <tupy77@hotmail.com>
2023-06-08 23:33:58 +0200

    middlewares

--- main.py
+++ main.py
@@ -3,23 +3,21 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
 from jwt_manager import create_token, validate_token
-from fastapi.security import HTTPBearer
 from config.database import Session, engine, Base
+from middlewares.error_handler import ErrorHandler
+from middlewares.jwt_bearer import JWTBearer
 from models.movie import Movie as MovieModel
 from fastapi.encoders import jsonable_encoder
+
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 
+app.add_middleware(ErrorHandler)
+
 Base.metadata.create_all(bind=engine)
 
-class JWTBearer(HTTPBearer):
-    async def __call__(self, request: Request):
-        auth = await super().__call__(request)
-        data = validate_token(auth.credentials)
-        if data['email'] != "admin@gmail.com":
-            raise HTTPException(status_code=403, detail="Credenciales son invalidas")
 
 class User(BaseModel):
     email:str
@@ -78,24 +76,22 @@ def login(user: User):
 @app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
     db = Session()
-    movies = db.query(MovieModel).all()
-    return JSONResponse(status_code=200, content=jsonable_encoder(movies))
+    result = db.query(MovieModel).all()
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 @app.get('/movies/{id}', tags=['movies'], response_model=Movie)
 def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     db = Session()
-    movie = db.query(MovieModel).filter(MovieModel.id == id).first()
-    if movie:
-        return JSONResponse(status_code=200, content=jsonable_encoder(movie))
-    return JSONResponse(status_code=404, content={"message": "No se encontró la película"})
-
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404, content={'message': "No encontrado"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 @app.get('/movies/', tags=['movies'], response_model=List[Movie])
 def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
     db = Session()
-    data = db.query(MovieModel).filter(MovieModel.category == category).all()
-    
-    return JSONResponse(status_code=200, content=jsonable_encoder(data))
+    result = db.query(MovieModel).filter(MovieModel.category == category).all()
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 @app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
 def create_movie(movie: Movie) -> dict:
@@ -108,17 +104,15 @@ def create_movie(movie: Movie) -> dict:
 @app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
 def update_movie(id: int, movie: Movie)-> dict:
     db = Session()
-    #db.query(MovieModel).filter(MovieModel.id == id).update(movie.dict())
     result = db.query(MovieModel).filter(MovieModel.id == id).first()
     if not result:
-        return JSONResponse(status_code=404, content={"message": "No se encontró la película"})
+        return JSONResponse(status_code=404, content={'message': "No encontrado"})
     result.title = movie.title
     result.overview = movie.overview
     result.year = movie.year
     result.rating = movie.rating
     result.category = movie.category
     db.commit()
-    
     return JSONResponse(status_code=200, content={"message": "Se ha modificado la película"})
 
 @app.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
@@ -126,12 +120,10 @@ def delete_movie(id: int)-> dict:
     db = Session()
     result = db.query(MovieModel).filter(MovieModel.id == id).first()
     if not result:
-        return JSONResponse(status_code=404, content={"message": "No se encontró la película"})
+        return JSONResponse(status_code=404, content={'message': "No encontrado"})
     db.delete(result)
     db.commit()
     return JSONResponse(status_code=200, content={"message": "Se ha eliminado la película"})
-
-
 
 '''
 {
