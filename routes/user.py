5a134fd5d1b18638d7ee5ff13aaff4b1c702a994
tupy77 <tupy77@hotmail.com>
2023-06-09 17:11:39 +0200

    routes

---
+++ routes/user.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from jwt_manager import create_token
+
+
+user_router = APIRouter()
+
+class User(BaseModel):
+    email:str
+    password:str
+
+
+@user_router.post('/login', tags=['auth'])
+def login(user: User):
+    if user.email == "admin@gmail.com" and user.password == "admin":
+        token: str = create_token(user.dict())
+        return JSONResponse(status_code=200, content=token)
