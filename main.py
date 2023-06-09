475c22cafe3901ae833fc0ee0d5d7859e30467fd
tupy77 <tupy77@hotmail.com>
2023-06-09 18:03:42 +0200

    servicios

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from pydantic import BaseModel
 from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 
-from routes.movie import movie_router, user_router
+from routes.movie import movie_router
+from routes.user import user_router
+
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 
 app.add_middleware(ErrorHandler)
-app.include_router([movie_router, user_router])
+app.include_router(movie_router)
+app.include_router(user_router)
 
 Base.metadata.create_all(bind=engine)
 
--- routes/movie.py
+++ routes/movie.py
@@ -8,6 +8,7 @@ from config.database import Session
 from middlewares.jwt_bearer import JWTBearer
 from models.movie import Movie as MovieModel
 from fastapi.encoders import jsonable_encoder
+from services.movie import MovieService
 
 movie_router = APIRouter()
 
@@ -32,16 +33,17 @@ class Movie(BaseModel):
         }
 
 
-@movie_router.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
+#@movie_router.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
+@movie_router.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200)
 def get_movies() -> List[Movie]:
     db = Session()
-    result = db.query(MovieModel).all()
+    result = MovieService(db).get_movies()
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 @movie_router.get('/movies/{id}', tags=['movies'], response_model=Movie)
 def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     db = Session()
-    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    result = MovieService(db).get_movie(id)
     if not result:
         return JSONResponse(status_code=404, content={'message': "No encontrado"})
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
