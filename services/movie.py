a651043deb52ddd7cf7e14f75f29f45f6f180621
tupy77 <tupy77@hotmail.com>
2023-06-09 18:29:17 +0200

    mas servicios y schemas

--- services/movie.py
+++ services/movie.py
@@ -12,3 +12,31 @@ class MovieService():
         result = self.db.query(MovieModel).filter(MovieModel.id == id).first()
         return result
     
+    def get_movies_by_category(self, category: str):
+        result = self.db.query(MovieModel).filter(MovieModel.category == category).all()
+        return result
+    
+    def create_movie(self, movie: MovieModel):
+        self.db.add(movie)
+        self.db.commit()
+        return True
+    
+    def update_movie(self, id: int, movie: MovieModel):
+        result = self.db.query(MovieModel).filter(MovieModel.id == id).first()
+        if not result:
+            return False
+        result.title = movie.title
+        result.overview = movie.overview
+        result.year = movie.year
+        result.rating = movie.rating
+        result.category = movie.category
+        self.db.commit()
+        return True
+    
+    def delete_movie(self, id: int):
+        result = self.db.query(MovieModel).filter(MovieModel.id == id).first()
+        if not result:
+            return False
+        self.db.delete(result)
+        self.db.commit()
+        return True
